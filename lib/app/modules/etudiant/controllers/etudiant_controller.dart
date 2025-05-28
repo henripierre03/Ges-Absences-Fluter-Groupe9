40bbeb6d84c4a58176d2f66eaf58a3599c3029de
Ameth Ba <amethba8826@gmail.com>
2025-05-28 10:56:32 +0000

    `add new folders`

--- lib/app/modules/etudiant/controllers/etudiant_controller.dart
+++ lib/app/modules/etudiant/controllers/etudiant_controller.dart
@@ -4,20 +4,8 @@ class EtudiantController extends GetxController {
   //TODO: Implement EtudiantController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   void increment() => count.value++;
 }
